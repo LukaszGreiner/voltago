53d5ff70871cbd7c85b9a471e21ba51fcddd4b63
Łukasz Greiner <lukaszgreiner@iMac-arris.local>
2025-01-20 09:52:41 +0100

    footer RWD for large screens

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,9 +5,16 @@ import FooterNav from "./Footer/FooterNav.vue";
 <template>
   <!-- Footer -->
   <footer class="pt-10 flex flex-col items-left justify-center">
-    <div class="px-4">
-      <div class="grid gap-4 mb-12 items-left md:justify-center">
-        <h2 class="text-[32px] leading-[44px] mb-6">Du hast noch Fragen?</h2>
+    <div
+      class="px-4 min-[1300px]:px-20 lg:flex lg:flex-row-reverse lg:justify-end"
+    >
+      <!-- Text -->
+      <div
+        class="grid gap-4 mb-12 items-left md:justify-center lg:absolute lg:left-1/2 lg:transform lg:-translate-x-1/2"
+      >
+        <h2 class="text-2xl md:text-[32px] md:leading-[44px] mb-2">
+          Du hast noch Fragen?
+        </h2>
 
         <div class="flex flex-col gap-4 md:gap-16 md:flex-row">
           <div class="grid">
@@ -33,6 +40,8 @@ import FooterNav from "./Footer/FooterNav.vue";
           </div>
         </div>
       </div>
+
+      <!-- Img -->
       <div class="flex justify-center">
         <img
           class="h-[129px] w-[200px] md:h-[207px] md:w-[324px]"
--- src/components/Footer/FooterNav.vue
+++ src/components/Footer/FooterNav.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="py-7 flex justify-center bg-tlo-ciemne-2">
-    <ul class="flex text-base leading-5 gap-10 bg-tlo-ciemne-2">
+    <ul class="flex text-base leading-5 gap-10 bg-tlo-ciemne-2 font-normal">
       <li><a href="/impressum">Impressum</a></li>
       <li><a href="/datenschutz">Datenschutz</a></li>
       <li><a href="/cookies">Cookies</a></li>
