bf3dd5aa9559bdf4c9e6c058cd06a3539b8026cb
xKaktus <greinerlukasz@gmail.com>
2025-04-09 11:34:32 +0200

    Fixed import

--- src/components/Footer/FooterNav.vue
+++ src/components/Footer/FooterNav.vue
@@ -1,9 +1,9 @@
 <template>
   <nav class="py-7 flex justify-center bg-tlo-ciemne-2">
     <ul class="flex text-base leading-5 gap-10 bg-tlo-ciemne-2 font-normal">
-      <li><a href="/impressum">Impressum</a></li>
-      <li><a href="/datenschutz">Datenschutz</a></li>
-      <li><a href="/cookies">Cookies</a></li>
+      <li><a href="#">Impressum</a></li>
+      <li><a href="#">Datenschutz</a></li>
+      <li><a href="#">Cookies</a></li>
     </ul>
   </nav>
 </template>
--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LandingPage from "@/views/LandingPage.vue";
 import YourScooterPage from "@/views/YourScooterPage.vue";
-import AccessoriesPage from "./views/Accessoriespage.vue";
+import AccessoriesPage from "@/views/AccessoriesPage.vue";
 import OrderDetailsPage from "@/views/OrderDetailsPage.vue";
 import SummaryPage from "@/views/SummaryPage.vue";
 import TestingComponentsPage from "@/views/TestingComponentsPage.vue";
